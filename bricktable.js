8a97dcb107f73c137307945ca997c229855db7fd
Artur Z <arjamizo@gmail.com>
2014-08-23 00:37:52 +0200

    let's say, delayed execution for every row

--- bricktable.js
+++ bricktable.js
@@ -1,10 +1,16 @@
 $('table').each(function(i, table) {
-    $(table).find('td').parent().filter(':odd').find('td').each((function() {
+
+    //this function is executed once per every table, so widths are calculated only once. 
+    var rowsFunction = (function() {
         var cache = [];
 
         return function(i, e) {
             e.style['position'] = 'relative';
             e.style['left'] = cache[i] || (cache[i] = (e.clientWidth || $(e).width()) / 2) + 'px';
         }
-    })())
+    })()
+
+    $(table).find('td').parent().filter(':odd').find('td').each(function(i, e) {
+        setTimeout(rowsFunction, 100, i, e);
+    })
 })
